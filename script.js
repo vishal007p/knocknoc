020e4e3d8faef69ff9dca43114c5f64a5af751d8
vishal007p <107975264+vishal007p@users.noreply.github.com>
2022-11-02 18:53:56 +0530

    new code

--- script.js
+++ script.js
@@ -69,4 +69,52 @@ options = {
 
 
 
+//  ---------conter-----
+
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+   let obj = document.getElementById(id),
+    current = start,
+    range = end - start,
+    increment = end > start ? 1 : -1,
+    step = Math.abs(Math.floor(duration / range)),
+    timer = setInterval(() => {
+     current += increment;
+     obj.textContent = current;
+     if (current == end) {
+      clearInterval(timer);
+     }
+    }, step);
+  }
+  counter("count1", 0, 42, 3000);
+  counter("count2", 100, 2100, 2500);
+  counter("count3", 0, 900, 3000);
+  counter("count4", 0, 6, 3000);
+  counter("count5", 0, 4, 3000);
+ });
  
+
+
+
+ $(document).ready(function(){
+  $('.customer-logos').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [{
+          breakpoint: 768,
+          settings: {
+              slidesToShow: 4
+          }
+      }, {
+          breakpoint: 520,
+          settings: {
+              slidesToShow: 3
+          }
+      }]
+  });
+});
